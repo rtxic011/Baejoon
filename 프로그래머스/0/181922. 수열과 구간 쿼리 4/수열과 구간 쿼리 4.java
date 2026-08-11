e0ffbe10a9dab4636144880e1810177def27201a
rtxic011 <kyukami7@dgsw.hs.kr>
2026-08-11 12:05:37 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.17 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] q : queries) {
+            for (int i = q[0]; i <= q[1]; i++) {
+                if (q[2] != 0 && i % q[2] == 0) {
+                    arr[i]++;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
